ce9f44015168df351c80508108145bb46091b264
Paludeto <gpalu54@gmail.com>
2022-03-31 18:28:10 -0300

    Add all files, API is still a WIP

---
+++ demo/src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,37 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student Cleiton = new Student(
+						"Cleiton",
+						"Cleitinho@gmail.com",
+						LocalDate.of(2003, Month.JANUARY, 1),
+						19
+            );
+
+            Student Rasta = new Student(
+						"Rasta",
+						"Rasta@gmail.com",
+						LocalDate.of(2003, Month.FEBRUARY, 2),
+						19
+            );
+
+            repository.saveAll(
+                    List.of(Cleiton, Rasta)
+            );
+
+        };
+    }
+}
